6d1c66e050b1de3384d80743483a9ea2ddd1bba9
Wallace Oliveira <wallaceanalista86@gmail.com>
2023-03-28 21:56:12 -0300

    Meu primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 's2z8kp',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
