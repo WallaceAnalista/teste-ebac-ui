99aca99510550602bac769038c0ab7b0fb29fee6
Wallace Oliveira <wallaceanalista86@gmail.com>
2023-03-28 23:12:12 -0300

    Meu segundo commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,35 +1,40 @@
 /// <reference types="cypress" />
 
-context('Funcionalidade Login', () =>{
+context('Funcionalidade Login', () => {
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });    
+    
+
+    it('Deve fazer login com sucesso', () => {        
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.page-title').should('contain', 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno? Sair)' )
-
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno? Sair)')
 
     })
 
-    it('deve exibir uma mensagem de erro ao inserir usuario inválidos', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it('deve exibir uma mensagem de erro ao inserir usuario inválidos', () => {        
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
-        cy.get('.woocommerce-form > .button').click()  
-        
+        cy.get('.woocommerce-form > .button').click()
+
         cy.get('.woocommerce-error').should('contain', 'a senha fornecida para o e-mail')
-        
+
     })
 
-    it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {        
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
-        cy.get('.woocommerce-form > .button').click() 
-        
+        cy.get('.woocommerce-form > .button').click()
+
         cy.get('.woocommerce-error').should('contain', 'a senha fornecida para o e-mail')
     })
 
