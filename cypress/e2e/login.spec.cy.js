a3284719680c29aac4bcafe7f45e0a269cd63e7d
WallaceAnalista <wallaceanalista86@gmail.com>
2023-08-30 11:44:49 -0300

    alterações no teste automatizado

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,16 @@
 /// <reference types="cypress" />
 
 context('Funcionalidade Login', () =>{
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')        
+    });
 
-    it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    afterEach(() => {
+        cy.screenshot()
+    });
+       
+    it('Deve fazer login com sucesso', () => {        
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
